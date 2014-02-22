6dee99715cbb5c6dcb3b387870bc7f4f5bf8faa0
Shaw Innes <shaw@immortal.net.au>
2014-02-22 19:22:41 +1100

    FeatureSwitch

---
+++ Grunch.Data/Migrations/201402220810313_Authentication.cs
@@ -0,0 +1,16 @@
+namespace Grunch.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Authentication : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ WebSite/Extensions/ViewExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Grunch.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite
+{
+    public static class View
+    {
+        public static IFeatureService FeatureService { get; set; }
+
+        public static bool HasFeature(Feature feature)
+        {
+            return FeatureService.HasFeature(feature);
+        }
+    }
+}
