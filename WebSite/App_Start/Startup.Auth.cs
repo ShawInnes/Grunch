1dc71227eb393df74c5ffcd077da1b0d8b8fe2ed
Shaw Innes <shaw@immortal.net.au>
2014-02-23 13:27:30 +1100

    Enable All Authentication Providers If Configured

--- WebSite/App_Start/Startup.Auth.cs
+++ WebSite/App_Start/Startup.Auth.cs
@@ -10,27 +10,41 @@ namespace WebSite
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            Grunch.Services.IConfigurationManagerService configurationManager = new Grunch.Services.ConfigurationManagerService();
+
             // Enable the application to use a cookie to store information for the signed in user
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login")
             });
+
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-            // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            if (!string.IsNullOrEmpty(configurationManager.AppSettings.Get("auth:MicrosoftClientId")) &&
+                    !string.IsNullOrEmpty(configurationManager.AppSettings.Get("auth:MicrosoftClientId")))
+            {
+                app.UseMicrosoftAccountAuthentication(
+                    clientId: configurationManager.AppSettings.Get("auth:MicrosoftClientId"),
+                    clientSecret: configurationManager.AppSettings.Get("auth:MicrosoftClientId"));
+            }
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            if (!string.IsNullOrEmpty(configurationManager.AppSettings.Get("auth:TwitterConsumerKey")) &&
+                    !string.IsNullOrEmpty(configurationManager.AppSettings.Get("auth:TwitterConsumerSecret")))
+            {
+                app.UseTwitterAuthentication(
+                   consumerKey: configurationManager.AppSettings.Get("auth:TwitterConsumerKey"),
+                   consumerSecret: configurationManager.AppSettings.Get("auth:TwitterConsumerSecret"));
+            }
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            if (!string.IsNullOrEmpty(configurationManager.AppSettings.Get("auth:FacebookAppId")) &&
+                    !string.IsNullOrEmpty(configurationManager.AppSettings.Get("auth:FacebookAppSecret")))
+            {
+                app.UseFacebookAuthentication(
+                   appId: configurationManager.AppSettings.Get("auth:FacebookAppId"),
+                   appSecret: configurationManager.AppSettings.Get("auth:FacebookAppSecret"));
+            }
 
             app.UseGoogleAuthentication();
         }
